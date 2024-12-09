1a8fefd4006835bec38ac8321bb9e9d712d24f7b
SHAIK RESHMA <shaikreshma1516@gmail.com>
2024-12-09 20:57:52 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,102 @@
+
+const apikey = '940535d9012c562cdc9a3d7eaafe1f2c';
+const locationInput = document.getElementById('location-input');
+const searchButton = document.getElementById('search-button');
+const weatherOutput = document.getElementById('weather-output');
+
+const locations = ["Tirupati", "Hyderabad","Bangalore", "Chennai"];
+
+
+locationInput.addEventListener('input', function() {
+    let val = this.value;
+    closeAllList();
+    if (!val) return;
+
+
+
+    let autocompleteItems = document.createElement('div');
+    autocompleteItems.setAttribute('class', 'autocomplete-items');
+    this.parentNode.appendChild(autocompleteItems);
+
+    locations.forEach(location => {
+        if (location.substr(0, val.length).toUpperCase() === val.toUpperCase()) {
+            let item = document.createElement('div');
+            item.innerHTML = "<strong>" + location.substr(0, val.length) + "</strong>";
+            item.innerHTML += location.substr(val.length);
+            item.innerHTML += "<input type='hidden' value='" + location + "'>";
+            item.addEventListener('click', function() {
+                locationInput.value = this.getElementsByTagName('input')[0].value;
+                closeAllList();
+            });
+
+            autocompleteItems.appendChild(item);
+        }
+    });
+});
+
+function closeAllList(elmnt) {
+    let items = document.getElementsByClassName('autocomplete-items');
+    for (let i = 0; i < items.length; i++) {
+        if (elmnt !== items[i] && elmnt !== locationInput) {
+            items[i].parentNode.removeChild(items[i]);
+        }
+    }
+}
+
+
+
+document.addEventListener('click', function(e) {
+    closeAllList(e.target);
+});
+
+searchButton.addEventListener('click', function() {
+    let location = locationInput.value;
+
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+async function fetchWeather(location) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=metric`);
+        if (!response.ok) throw new Error('Location not found');
+        const weatherData = await response.json();
+        displayWeather(weatherData);
+        
+    } catch (error) {
+        weatherOutput.innerHTML = `<p>${error.message}</p>`;
+    }
+}
+
+function displayWeather(data) {
+    const { name, main, weather } = data;
+    weatherOutput.innerHTML =
+        `<p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+            </svg>Location: ${name}</p>
+
+
+        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-sun" viewBox="0 0 16 16">
+        <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V2.5a.5.5 0 0 1 1 0v8.585A1.5 1.5 0 0 1 5 12.5"/>
+        <path d="M1 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0zM3.5 1A1.5 1.5 0 0 0 2 2.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0L5 10.486V2.5A1.5 1.5 0 0 0 3.5 1m5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5m4.243 1.757a.5.5 0 0 1 0 .707l-.707.708a.5.5 0 1 1-.708-.708l.708-.707a.5.5 0 0 1 .707 0M8 5.5a.5.5 0 0 1 .5-.5 3 3 0 1 1 0 6 .5.5 0 0 1 0-1 2 2 0 0 0 0-4 .5.5 0 0 1-.5-.5M12.5 8a.5.5 0 0 1 .5-.5h1a.5.5 0 1 1 0 1h-1a.5.5 0 0 1-.5-.5m-1.172 2.828a.5.5 0 0 1 .708 0l.707.708a.5.5 0 0 1-.707.707l-.708-.707a.5.5 0 0 1 0-.708M8.5 12a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5"/>
+        </svg>Temperature: ${main.temp}°C</p>
+
+
+        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-sun-fill" viewBox="0 0 16 16">
+        <path d="M11.473 11a4.5 4.5 0 0 0-8.72-.99A3 3 0 0 0 3 16h8.5a2.5 2.5 0 0 0 0-5z"/>
+        <path d="M10.5 1.5a.5.5 0 0 0-1 0v1a.5.5 0 0 0 1 0zm3.743 1.964a.5.5 0 1 0-.707-.707l-.708.707a.5.5 0 0 0 .708.708zm-7.779-.707a.5.5 0 0 0-.707.707l.707.708a.5.5 0 1 0 .708-.708zm1.734 3.374a2 2 0 1 1 3.296 2.198q.3.423.516.898a3 3 0 1 0-4.84-3.225q.529.017 1.028.129m4.484 4.074c.6.215 1.125.59 1.522 1.072a.5.5 0 0 0 .039-.742l-.707-.707a.5.5 0 0 0-.854.377M14.5 6.5a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"/>
+        </svg>Weather: ${weather[0].description}</p>
+
+
+        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
+        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+        </svg> Humidity: ${main.humidity}%</p>`;
+}
+
+
+
+
+
+
+
